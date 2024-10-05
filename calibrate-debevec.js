30f5ece726b5736beb0d00fac582943f64cf171c
Anthony Gagnon <59715170+dany-demise@users.noreply.github.com>
2024-10-05 19:09:36 -0400

    Update calibrate-debevec.js

--- calibrate-debevec.js
+++ calibrate-debevec.js
@@ -1,1 +1,11 @@
+// Create two vectors
+const { Vector, Matrix } = linear_algebra;
+const v1 = new Vector([1, 2, 3]);
+const v2 = new Vector([4, 5, 6]);
 
+// Perform vector operations
+const sum = v1.add(v2);
+const dotProduct = v1.dot(v2);
+
+console.log('Vector sum:', sum);
+console.log('Dot product:', dotProduct);
